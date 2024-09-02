a1601da420f06e561ffc25ab473ae31a06779343
salman1256 <salman1256@yahoo.com>
2024-09-02 15:35:42 +0530

    MyBrach Changes

---
+++ Controllers/MyController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPISecure.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MyController : ControllerBase
+    {
+    }
+}
